9b6e70de2f23762ae53ae2d6d6cc0a256ad0fc40
akhil <aelamanc@calpoly.edu>
2023-10-09 02:20:32 -0700

    linked frontend with backend

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  function fetchUsers(){
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   function removeOneCharacter(index){
     const updated = characters.filter((characters, i) => {
       return i !== index
@@ -11,9 +29,25 @@ function MyApp() {
     setCharacters(updated)
   }
 
-  function updateList(person){
-    setCharacters([...characters, person]);
-  }
+  // function updateList(person){
+  //   setCharacters([...characters, person]);
+  // }
+
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+      console.log(error);
+    })
+}
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
+
   return (
     <div className="container">
       <Table characterData={characters} 

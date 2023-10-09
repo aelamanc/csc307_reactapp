3cc2bab37c10dcd4cdc6f6fdad7acde948fcbe17
akhil <aelamanc@calpoly.edu>
2023-10-09 01:05:11 -0700

    added post method for a user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,6 +39,24 @@ const findUserByName = (name) => {
         .filter( (user) => user['name'] === name); 
 }
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+
+        app.get('/users/:id', (req, res) => {
+            const id = req.params['id']; //or req.params.id
+            let result = findUserById(id);
+            if (result === undefined) {
+                res.status(404).send('Resource not found.');
+            } else {
+                res.send(result);
+            }
+        });
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+        }
+
 app.get('/users', (req, res) => {
     const name = req.query.name;
     if (name != undefined){
@@ -49,6 +67,16 @@ app.get('/users', (req, res) => {
     else{
         res.send(users);
     }
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
 });
 
 app.listen(port, () => {
